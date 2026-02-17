4abee3a5fdeed0b3bc5655d6f43003cd204098a0
ranroby76 <ranroby76@gmail.com>
2026-02-17 04:29:48 +0200

    Delete backup code_vst2+drag_n_drop directory

--- backup code_vst2+drag_n_drop/src/PluginManagerTab_CompileUnit.cpp
+++
@@ -1,9 +0,0 @@
-// Compile unit marker - ensures all split files are linked together
-
-// This file exists to satisfy the CMake build system.
-// All PluginManagerTab implementations are in the split files:
-// - PluginManagerTab_Main.cpp
-// - PluginManagerTab_TreeItem.cpp
-// - PluginManagerTab_Folders.cpp
-// - PluginManagerTab_AutoScanner.cpp
-// - PluginManagerTab_ScanOptions.cpp
